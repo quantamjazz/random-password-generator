b72d025462925e21e7477881f45f01bca0103598
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2023-12-16 12:53:17 +0000

    Created getPasswordOptions function and called it, tested prompts, currently working

--- starter/script.js
+++ starter/script.js
@@ -33,155 +33,124 @@ function getRandom(arr) {
   // So then, mega-array[generated-index] is the actual character
   // Add that character to the password
 
-  // Once we finish the for loop, return the generated password
-}
+  // Once we finish the for loop, return the generated password*/
 
 // Array of special characters to be included in password
 const specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 const lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 const upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-// Function to prompt user for password options
 function getPasswordOptions() {
+  const length = parseInt(prompt("How many characters would you like your password to contain? Enter a number between 8 and 128."));
 
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-
-}
-
-// Get references to the #generate element
-const generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-// Function to get a random element from an array
-function getRandom(arr) {
-  var randomIndex = Math.floor(Math.random() * arr.length);
-  var randomElement = arr[randomIndex];
-  return randomElement;
-}
-
-// Main function to generate a password
-function generatePassword() {
-  var passwordLength = 8; // You can prompt the user for a length
-  var password = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-
-  for (var i = 0; i < passwordLength; i++) {
-    password += getRandom(characters.split(''));
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Password length must be between 8 and 128 characters");
+    return null;
   }
 
-  return password;
-}
+  const includeLowercase = confirm("Click OK to confirm including lowercase characters.");
+  const includeUppercase = confirm("Click OK to confirm including uppercase characters.");
+  const includeNumbers = confirm("Click OK to confirm including numeric characters.");
+  const includeSpecial = confirm("Click OK to confirm including special characters.");
 
-// Function to write the password to the textarea
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-  passwordText.value = password;
-}
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+    alert("Must select at least one character type");
+    return null;
+  }
 
-// Add event listener to generate button
-var generateBtn = document.querySelector('#generate');
-generateBtn.addEventListener('click', writePassword);
+  return {
+    length: length,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+    includeNumbers: includeNumbers,
+    includeSpecial: includeSpecial
+  };
+
+  }
+
+  getPasswordOptions();
